aab2c8ef68adb9d1b04b5decd3a3629b0d25ab3d
CrossBon3 <59312099+CrossBon3@users.noreply.github.com>
2020-04-16 19:31:26 +0530

    Update cnfg.py

--- cnfg.py
+++ cnfg.py
@@ -1,18 +1,36 @@
 import os
-# screen size to pass to pygame display screen
+# screen size to pass to game display screen
 SCREENSIZE = [640, 640]
 
-# color data to pass to maintanance unit
-BACKGROUND = (0, 0, 0)
-FONT_COLOR = (0, 255, 255)
+# color data to pass to game world
+BACKGROUND_BLACK = (0, 0, 0)
+
+BACKGROUND_START_SCREEN = (60, 60, 60)
+
+TITLE_FONT_COLOR = (0, 255, 255)
+
+CONTENT_FONT_COLOR = (255, 255, 0)
+
+# frame rate of the game
+FRAME_RATE = 30
+
+# font path to pass to start screen interface
+TITLE_FONT_PATH = os.path.join(os.getcwd(), 'resources/fonts/OpenSans-Bold.ttf')
+
+CONTENT_FONT_PATH = os.path.join(os.getcwd(), 'resources/fonts/OpenSans-Italic.ttf')
+
 
 # image paths to pass to main game unit(game_start.py)
-player_IMAGE_PATH = os.path.join(os.getcwd(), 'resources/you.jpg')
+player_IMAGE_PATH = os.path.join(os.getcwd(), 'resources/png/you.png')
 
-DESTINATION_IMAGE_PATH = os.path.join(os.getcwd(), 'resources/me.jpg')
+DESTINATION_IMAGE_PATH = os.path.join(os.getcwd(), 'resources/png/me.png')
 
-OBSTACLE_IMAGE_PATH = [os.path.join(os.getcwd(), 'resources/r.jpg'),
-                       os.path.join(os.getcwd(), 'resources/x.jpg'),
-                       os.path.join(os.getcwd(), 'resources/y.jpg'),
-                       os.path.join(os.getcwd(), 'resources/p.jpg')
-                       ]
+OBSTACLE_IMAGE_R_PATH = os.path.join(os.getcwd(), 'resources/png/r.png')
+
+OBSTACLE_IMAGE_X_PATH = os.path.join(os.getcwd(), 'resources/png/x.png')
+
+OBSTACLE_IMAGE_Y_PATH = os.path.join(os.getcwd(), 'resources/png/y.png')
+
+OBSTACLE_IMAGE_P_PATH = os.path.join(os.getcwd(), 'resources/png/p.png')
+
+OBSTACLE_IMAGE_F_PATH = os.path.join(os.getcwd(), 'resources/png/f.png')
